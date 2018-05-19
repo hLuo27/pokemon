1fa93b53945936564792e3f2956044eaa97cafcb
Hubert Luo <hubertluo@berkeley.edu>
2018-05-19 19:31:45 -0400

    Finish splitting data

--- classify_legenday_pokemon.R
+++ classify_legenday_pokemon.R
@@ -2,14 +2,23 @@ library(dplyr)
 
 pokemon = read.csv('pokemon.csv')
 
-# Features selected are base total, capture rate, and experience growth. Name and legenadry status kept for identification purposes.
+# Features selected are base total, capture rate, and experience growth. Name, pokedex number, and legenadry status kept for identification purposes.
 
-pokemon = pokemon %>% select(name, base_total, capture_rate, experience_growth, is_legendary)
+pokemon = pokemon %>% select(name, pokedex_number, base_total, capture_rate, experience_growth, is_legendary)
 head(pokemon)
 
 n = nrow(pokemon) # total number of pokemon in pokedex
 
 # We divide the data into an 80-20 split between training and testing data
 
-training_indicies = sample(1:n, size = round(0.8*n), replace = FALSE) # Indicies of pokemon to use for training set
-test_indicies = 1:n - training_indicies
+set.seed(123)
+training_indices = sort(sample(1:n, size = round(0.8*n), replace = FALSE))
+testing_indices = numeric()
+for (i in 1:n){
+  if (!(i %in% training_indices)){
+    testing_indices = c(testing_indices, i)
+  }
+}
+
+training = pokemon[training_indices,]
+testing = pokemon[testing_indices,]
