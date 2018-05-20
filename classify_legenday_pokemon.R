6d9a5f954d3dbbf612c77fd0a8fbc7ffd222addf
Hubert Luo <hubertluo@berkeley.edu>
2018-05-19 20:18:50 -0400

    Complete classifier

--- classify_legenday_pokemon.R
+++ classify_legenday_pokemon.R
@@ -1,11 +1,23 @@
 library(dplyr)
 
-pokemon = read.csv('pokemon.csv')
+pokemon = read.csv('pokemon.csv', stringsAsFactors = FALSE)
 
 # Features selected are base total, capture rate, and experience growth. Name, pokedex number, and legenadry status kept for identification purposes.
 
 pokemon = pokemon %>% select(name, pokedex_number, base_total, capture_rate, experience_growth, is_legendary)
 head(pokemon)
+
+# Now we normalize each of the features
+normalize_feature <- function(x){
+  x = as.integer(x)
+  (x-mean(x,na.rm = TRUE))/sd(x, na.rm = TRUE)
+}
+
+for (i in 3:5){
+  pokemon[,i] = normalize_feature(pokemon[,i]) # Note: Minior has a string for capture rate so that value is coerced to NA
+}
+
+pokemon = pokemon %>% filter(!is.na(capture_rate)) # Remove 1 row with missing value (Minior)
 
 n = nrow(pokemon) # total number of pokemon in pokedex
 
@@ -21,4 +33,23 @@ for (i in 1:n){
 }
 
 training = pokemon[training_indices,]
-testing = pokemon[testing_indices,]
+testing = pokemon[testing_indices,] 
+
+# Distance function to calculate distance between features of two pokemon
+
+distance <- function(train_row = training[1,], test_row = testing[1,]){
+  total = 0
+  for (i in 3:5){
+    total = total + (test_row[,i] - train_row[,i])^2
+  }
+  return(sqrt(total))
+}
+
+# Classify row (x) in testing set
+
+classify <- function(x){
+  closest = training %>% mutate('Distance' = distance(training, x)) %>% arrange(Distance) %>% slice(1:3) # Select 3 closest pokemon in training set
+  names(which.max(table(closest$is_legendary)))
+}
+
+classify(testing[1,])
